2c754e2b63a43da22c2cc335e2152cf3b60aef97
Julius Sakki <j.sakki@icloud.com>
2022-09-27 17:30:29 +0300

    3.9

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const http = require('http')
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 
 app.use(express.json())
 //app.use(morgan('tiny'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(cors())
 
 morgan.token('body', (req, res) => {
   return JSON.stringify(req.body)
