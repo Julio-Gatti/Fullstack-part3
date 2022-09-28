c87e2e6bffee6b2a52d32408da765d92ec3a2ffd
Julius Sakki <j.sakki@icloud.com>
2022-09-28 18:46:59 +0300

    3.15

--- index.js
+++ index.js
@@ -6,61 +6,18 @@ const Person = require('./models/person')
 
 const app = express()
 
+app.use(express.static('build'))
 app.use(express.json())
-//app.use(morgan('tiny'))
+//app.use(requestLogger)
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(cors())
-app.use(express.static('build'))
 
 morgan.token('body', (req, res) => {
   return JSON.stringify(req.body)
 })
 
-/*let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456"
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523"
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345"
-  },
-  {
-    id: 4,
-    name: "Mary Poppendick",
-    number: "39-23-6423122"
-  }
-]*/
-
-app.get('/api/persons', (request, response) => {
-  // Database
-  Person.find({}).then(persons => {
-    response.json(persons)
-  })
-})
-
-app.get('/api/persons/:id', (request, response) => {
-  // Database
-  Person.findById(request.params.id).then(p => {
-    response.json(p)
-  })
-})
-
 app.post('/api/persons', (request, response) => {
   const body = request.body
-
-  /*if (body.content === undefined) {
-    return response.status(400).json({
-      error: 'Content missing'
-    })
-  }*/
 
   if (!body.name) {
     return response.status(400).json({
@@ -92,12 +49,52 @@ app.post('/api/persons', (request, response) => {
   })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(p => p.id !== id)
-
-  response.status(204).end()
+app.get('/api/persons', (request, response) => {
+  // Database
+  Person.find({})
+    .then(persons => {
+      response.json(persons)
+    })
 })
+
+app.delete('/api/persons/:id', (request, response) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+app.get('/api/persons/:id', (request, response, next) => {
+  // Database
+  Person.findById(request.params.id)
+    .then(p => {
+      if (p) {
+        response.json(p)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'Unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'Malformatted id' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler) // After all other middleware registrations
 
 app.get('/info', (request, response) => {
   const date = new Date()
@@ -105,7 +102,7 @@ app.get('/info', (request, response) => {
   response.send(`
     <p>The phonebook has info for ${persons.length} people</p>
     <p>${date}</p>
-    `)
+  `)
 })
 
 const PORT = process.env.PORT
